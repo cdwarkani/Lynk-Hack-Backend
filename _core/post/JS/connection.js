4d40190bf3534c6a3544e620903d9d05d351dc76
Chetan Dwarkani <mchetandwarkani@gmail.com>
2019-10-12 18:08:10 +0530

    new apis

--- _core/post/JS/connection.js
+++ _core/post/JS/connection.js
@@ -51,7 +51,8 @@ module.exports = {
 
 
         });
-    }, volunteerinsert: function (data) {
+    },
+    volunteerinsert: function (data) {
         return new Promise(function (resolve, reject) {
             var connection = mysql.createPool({
                 host: '139.59.94.48',
@@ -60,14 +61,15 @@ module.exports = {
                 database: 'LynkHack'
             });
             connection.getConnection(function (err) {
-                var queryValue = "select * from Volunteer";
+                var queryValue = "INSERT INTO `LynkHack`.`Volunteer` ( `PhoneNo`, `Name`, `Address`, `Latitude`, `Longitude`, `AreaID`, `createdOn`, `updatedOn`, `ipAddress`, `isActive`) VALUES (";
                 if (err) {
                     resolve({ "status": 400, "error": err });
                     connection.end();
                     return;
                 }
+
                 if (data.phoneNo) {
-                    queryValue += " where PhoneNo = " + data.phoneNo;
+                    queryValue += "'" + data.phoneNo+"',";
                 }else
                 {
                         resolve({ "status": 400, "error": "No phoneNo field found" });
@@ -75,9 +77,57 @@ module.exports = {
                         return;
                     
                 }
-                if (data.lowLimit && data.HighLimit) {
-                    queryValue += " LIMIT " + data.lowLimit + "," + data.HighLimit;
+
+                if (data.Name) {
+                    queryValue += "'" + data.Name+"',";
+                }else
+                {
+                        resolve({ "status": 400, "error": "No Name field found" });
+                        connection.end();
+                        return;
+                    
                 }
+
+                if (data.Address) {
+                    queryValue += "'" + data.Address+"',";
+                }else
+                {
+                        resolve({ "status": 400, "error": "No Address field found" });
+                        connection.end();
+                        return;
+                    
+                }
+
+                if (data.Latitude) {
+                    queryValue += "'" + data.Latitude+"',";
+                }else
+                {
+                        resolve({ "status": 400, "error": "No Latitude field found" });
+                        connection.end();
+                        return;
+                    
+                }
+                if (data.Longitude) {
+                    queryValue += "'" + data.Longitude+"',";
+                }else
+                {
+                        resolve({ "status": 400, "error": "No Longitude field found" });
+                        connection.end();
+                        return;
+                    
+                }
+
+                if (data.AreaID) {
+                    queryValue += "'" + data.AreaID+"',";
+                }else
+                {
+                        resolve({ "status": 400, "error": "No AreaID field found" });
+                        connection.end();
+                        return;
+                    
+                }
+                queryValue += "'2019-01-01 00:00:00','2019-01-01 00:00:00','1.1.1.1','1')";
+            
 
                 connection.query(queryValue, function (err, result, fields) {
                     if (err) {
@@ -85,13 +135,9 @@ module.exports = {
                         connection.end();
                         return;
                     }
-                    if (result.length && result.length > 0) {
+                    if (result) {
                         resolve({ "status": 200, "data": result, "isNewUser": false, "isSuccess": true });
-                    } else {
-                        resolve({ "status": 200, "isNewUser": true, "isSuccess": true });
                     }
-
-
                     connection.end();
                     return;
 
@@ -206,6 +252,111 @@ module.exports = {
                     }
                     resolve({ "status": 200, "data": result,  "isSuccess": true });
               
+                    connection.end();
+                    return;
+
+                });
+            });
+
+
+        });
+    },
+    creategroup: function (data) {
+        return new Promise(function (resolve, reject) {
+            var connection = mysql.createPool({
+                host: '139.59.94.48',
+                user: 'playtowindbuser',
+                password: 'Gameuser@123',
+                database: 'LynkHack'
+            });
+            connection.getConnection(function (err) {
+                var queryValue = "INSERT INTO `LynkHack`.`Groups` (`VolunteerID`, `AreaID`, `Name`, `Description`, `Members`, `link`, `createdOn`, `updatedOn`, `isCompleted`, `isActive`) VALUES (";
+                if (err) {
+                    resolve({ "status": 400, "error": err });
+                    connection.end();
+                    return;
+                }
+
+                if (data.VolunteerID) {
+                    queryValue += "'" + data.VolunteerID+"',";
+                }else
+                {
+                        resolve({ "status": 400, "error": "No VolunteerID field found" });
+                        connection.end();
+                        return;
+                    
+                }
+
+                if (data.AreaID) {
+                    queryValue += "'" + data.AreaID+"',";
+                }else
+                {
+                        resolve({ "status": 400, "error": "No AreaID field found" });
+                        connection.end();
+                        return;
+                    
+                }
+
+                if (data.Name) {
+                    queryValue += "'" + data.Name+"',";
+                }else
+                {
+                        resolve({ "status": 400, "error": "No Name field found" });
+                        connection.end();
+                        return;
+                    
+                }
+
+                if (data.Description) {
+                    queryValue += "'" + data.Description+"',";
+                }else
+                {
+                        resolve({ "status": 400, "error": "No Description field found" });
+                        connection.end();
+                        return;
+                    
+                }
+                if (data.Members) {
+                    queryValue += "'" + data.Members+"',";
+                }else
+                {
+                        resolve({ "status": 400, "error": "No Members field found" });
+                        connection.end();
+                        return;
+                    
+                }
+
+                if (data.link) {
+                    queryValue += "'" + data.link+"',";
+                }else
+                {
+                        resolve({ "status": 400, "error": "No AreaID field found" });
+                        connection.end();
+                        return;
+                    
+                }
+                queryValue += "'2019-01-01 00:00:00','2019-01-01 00:00:00'";
+                if (data.isCompleted) {
+                    queryValue += ",'" + data.isCompleted+"',";
+                }else
+                {
+                        resolve({ "status": 400, "error": "No isCompleted field found" });
+                        connection.end();
+                        return;
+                    
+                }
+                queryValue += "'1')";
+            
+
+                connection.query(queryValue, function (err, result, fields) {
+                    if (err) {
+                        resolve({ "status": 400, "error": err });
+                        connection.end();
+                        return;
+                    }
+                    if (result) {
+                        resolve({ "status": 200, "data": result, "isSuccess": true });
+                    }
                     connection.end();
                     return;
 
